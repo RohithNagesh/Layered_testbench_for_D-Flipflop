7fb277433af3c12df69e2a4b8db0281139a03e6e
RohithNagesh <103078929+RohithNagesh@users.noreply.github.com>
2023-08-05 19:23:04 +0530

    Add files via upload

---
+++ design.sv
@@ -0,0 +1,15 @@
+// Code your design here
+module DFF_ASRL(D,clk,reset,Q);
+  input bit[2:0] D;
+  input bit clk;
+  input bit reset;
+  output bit[2:0] Q;
+  
+always @(posedge clk or negedge reset) 
+begin
+ if(reset==1'b0)
+  Q <= 1'b0; 
+ else 
+  Q <= D; 
+end 
+endmodule
